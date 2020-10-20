648ac3013ba7c97600b9fe30f7222d9fc76d50f6
Aura29032008 <72656432+Aura29032008@users.noreply.github.com>
2020-10-20 11:43:25 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,39 @@
+var fixedRect, movingRect, aura1, aura2;
+
+function setup() {
+  createCanvas(1200,800);
+  fixedRect = createSprite(400, 100, 50, 80);
+  fixedRect.shapeColor = "green";
+  fixedRect.debug = true;
+
+  movingRect = createSprite(400, 800,80,30);
+  movingRect.shapeColor = "green";
+  movingRect.debug = true;
+
+  aura1=createSprite(600,500,60,50);
+  aura1.shapeColor="pink";
+  aura1.debug=true;
+  
+
+  aura2=createSprite(600,200,40,90);
+  aura2.shapeColor="blue";
+  aura2.debug=true;
+
+  movingRect.velocityY = -5;
+  fixedRect.velocityY = +5;
+  aura1.velocityY= -2;
+  aura2.velocityY= 2;
+}
+
+function draw() {
+  background(0,0,0);  
+  bounceOff(movingRect,fixedRect);
+
+  bounceOff(aura1,aura2);
+ 
+  drawSprites();
+
+}
+
+
+
